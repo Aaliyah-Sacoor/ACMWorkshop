ff12242375154588aeb3de75021751cf2f4b21f0
Aaliyah Sacoor <aaliyahsacoor@gmail.com>
2023-02-09 16:02:22 +0400

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean weather = true;
+        if(weather){
+           System.out.println("It is hot today");
+        }else{
+            System.out.println("It is cold today");
+        }
+        System.out.println("Hello world!");
+    }
+}
